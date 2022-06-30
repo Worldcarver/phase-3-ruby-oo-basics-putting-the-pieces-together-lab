08aa99e789cce01640471b3a1a9fef8fc07305d6
ian <ian.murphy92@gmail.com>
2022-06-30 10:26:40 -0400

    commit tree 6e82512ee669c2b94543069f1b4a8f45f70d9985

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    attr_accessor :page_count, :author, :title, :genre
 
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+# newBook = Book.new
